4bcaa738efdb828aea106b09523d11e69747136d
NOUAIM98 <142490737+NOUAIM98@users.noreply.github.com>
2025-09-13 19:33:01 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gray-900 text-white p-4 text-center mt-8">
-      <p>&copy; 2025 SilverConnect. All rights reserved.</p>
-    </footer>
+  <footer className="footer">
+  {/* SilverConnect. All rights reserved. */}
+</footer>
+
   );
 }
 
